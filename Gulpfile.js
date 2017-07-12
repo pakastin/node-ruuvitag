b25c5a231fbb334d0f81f9dd0002a0dcdc04e531
Espesen <esa.toivola@gmail.com>
2017-07-12 12:50:28 +0300

    Updated README.md

--- Gulpfile.js
+++ Gulpfile.js
@@ -17,40 +17,6 @@ gulp.task('jshint', function () {
     .pipe(jshint.reporter('default'));
 });
 
-// DONT COMMIT
-
-const raspi = require('./raspi-deploy.json');
-
-gulp.task('deploy', function () {
-
-  const remotePath = '/node/ruuvitest';
-
-  const raspi = require('./raspi-deploy.json');
-
-  const conn = ftp.create({
-    host: raspi.host,
-    user: raspi.user,
-    password: raspi.password,
-    parallel: 10,
-    log: gutil.log
-  });
-
-  const globs = [
-    'ruuvi.js',
-    'test.js',
-    'parse.js',
-    'dataformats/*.js',
-    'package.json'
-  ];
-
-  // using base = '.' will transfer everything to /public_html correctly
-  // turn off buffering in gulp.src for best performance
-
-  return gulp.src( globs, { base: '.', buffer: false } )
-    .pipe( conn.newer( remotePath ) ) // only upload newer files
-    .pipe( conn.dest( remotePath ) );
-});
-
 gulp.task('watch', ['jshint', 'test'], function () {
   gulp.watch(['**/*.js', '!node_modules/**/*.js'], ['jshint', 'test']);
 });
